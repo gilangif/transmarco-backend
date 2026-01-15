3e5cb1158deaaf6cfbdde9d2c57e63c60ca3f800
gilangif <gilangidulfitri1201@gmail.com>
2026-01-16 01:40:31 +0700

    change port

--- index.js
+++ index.js
@@ -12,7 +12,8 @@ const port = 1234
 updateSheets()
 checkOrders()
 
-app.use(cors())
+// app.use(cors())
+
 app.use(express.json({ limit: "200mb" }))
 app.use(express.urlencoded({ limit: "200mb", extended: true }))
 
