350e355e9e66dabeb67afb948c1e7090bcff138d
gilangif <gilangidulfitri1201@gmail.com>
2026-01-16 01:25:35 +0700

    initial

---
+++ index.js
@@ -0,0 +1,23 @@
+import express from "express"
+import axios from "axios"
+import cors from "cors"
+
+import errorHandler from "./middleware/errorHandler.js"
+import updateSheets from "./utils/updateSheets.js"
+import checkOrders from "./utils/checkOrders.js"
+import router from "./routes/router.js"
+
+const app = express()
+const port = 3000
+
+updateSheets()
+checkOrders()
+
+app.use(cors())
+app.use(express.json({ limit: "200mb" }))
+app.use(express.urlencoded({ limit: "200mb", extended: true }))
+
+app.use(router)
+app.use(errorHandler)
+
+app.listen(port, () => console.log(`# server listening on http://localhost:${port}`))
