1b7bca2ac49f6d7ee04fb36c9416fe4b73b7fe7b
gilangif <gilangidulfitri1201@gmail.com>
2026-04-17 22:58:14 +0700

    init

--- models/Sheets.js
+++ models/Sheets.js
@@ -8,7 +8,7 @@ class Sheets {
   constructor(brand) {
     const credentials = {
       HPAL: {
-        spreadsheetId: "15i9m8aY_eiHbqcxbEAJBpqLyonSnhQ00hgXP9rGR9nQ",
+        spreadsheetId: "16wFHDHFhEfD9HSXw7r7mcBI9WSHQQ737I9IG_44pGg0",
         type: "service_account",
         project_id: "studious-karma-439813-n7",
         private_key_id: "503c69d9802bb5e93b1948b26a575ce310759cb4",
